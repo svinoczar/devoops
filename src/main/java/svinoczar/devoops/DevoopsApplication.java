9f11a65a9b9b22f5aa67cf0c0fe71ad9622cd85b
svinoczar <alexsupper75@gmail.com>
2023-12-18 02:26:58 +0300

    project init

---
+++ src/main/java/svinoczar/devoops/DevoopsApplication.java
@@ -0,0 +1,13 @@
+package svinoczar.devoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevoopsApplication.class, args);
+    }
+
+}
